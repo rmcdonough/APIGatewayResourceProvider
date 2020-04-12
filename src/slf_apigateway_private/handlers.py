79e818e7cd0d530b267065623ae420e58488c006
Rich McDonough <mcdrich@amazon.com>
2020-04-12 11:44:59 -0400

    works now, needs refinement

--- src/slf_apigateway_private/handlers.py
+++ src/slf_apigateway_private/handlers.py
@@ -21,85 +21,132 @@ resource = Resource(TYPE_NAME, ResourceModel)
 test_entrypoint = resource.test_entrypoint
 
 
-@resource.handler(Action.CREATE)
-def create_handler(
-    session: Optional[SessionProxy],
-    request: ResourceHandlerRequest,
-    callback_context: MutableMapping[str, Any],
-) -> ProgressEvent:
-    model = request.desiredResourceState
-    progress: ProgressEvent = ProgressEvent(
-        status=OperationStatus.IN_PROGRESS,
-        resourceModel=model,
-    )
-    # TODO: put code here
+class APIGateway(object):
+    def __init__(self, session):
+        self.client = session.client('apigateway')
 
-    # Example:
+        #
+        # todo: make paginator safe
+        #
+
+        self.apis = []
+        apis = self.client.get_rest_apis()
+        for api_id in apis['items']:
+            self.apis.append(api_id['name'])
+
+    def list_apis(self):
+        """Returns the list of API Gateways"""
+        return self.apis
+
+    def create_api(self, name, description):
+        """
+        Create a new API Gateway
+
+        The endpoint configuration is hard coded to be PRIVATE
+        """
+        # Throw an error if we are trying to reuse an existing name
+        if name in self.apis:
+            raise NameError
+        response = self.client.create_rest_api(
+            name=name,
+            description=description,
+            endpointConfiguration={'types': ['PRIVATE']},
+            tags={
+                'CreatedBy': 'SLF::APIGateway::Private'
+            }
+        )
+        return response
+
+    def delete_api(self, name):
+        """Deletes an API Gateway"""
+        response = self.client.delete_rest_api(restApiId=name)
+        return response
+
+    def get_api(self, name):
+        """Returns information about an API"""
+        response = self.client.get_rest_api(restApiId=name)
+        return response
+
+
+@resource.handler(Action.CREATE)
+def create_handler(session: Optional[SessionProxy],
+                   request: ResourceHandlerRequest,
+                   callback_context: MutableMapping[str, Any]) -> ProgressEvent:
+    model = request.desiredResourceState
+    progress: ProgressEvent = ProgressEvent(status=OperationStatus.IN_PROGRESS, resourceModel=model)
     try:
-        if isinstance(session, SessionProxy):
-            client = session.client("s3")
-        # Setting Status to success will signal to cfn that the operation is complete
+        api = APIGateway(session)
+        response = api.create_api(model.Name, model.Description)
         progress.status = OperationStatus.SUCCESS
+        progress.resourceModel = {'restApiId': response['id'], 'Response': response}
     except TypeError as e:
-        # exceptions module lets CloudFormation know the type of failure that occurred
         raise exceptions.InternalFailure(f"was not expecting type {e}")
-        # this can also be done by returning a failed progress event
-        # return ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"was not expecting type {e}")
     return progress
 
 
 @resource.handler(Action.UPDATE)
-def update_handler(
-    session: Optional[SessionProxy],
-    request: ResourceHandlerRequest,
-    callback_context: MutableMapping[str, Any],
-) -> ProgressEvent:
+def update_handler(session: Optional[SessionProxy],
+                   request: ResourceHandlerRequest,
+                   callback_context: MutableMapping[str, Any]) -> ProgressEvent:
+    #
+    # todo: the update handler is just a stub for now
+    #
+
     model = request.desiredResourceState
     progress: ProgressEvent = ProgressEvent(
         status=OperationStatus.IN_PROGRESS,
         resourceModel=model,
     )
-    # TODO: put code here
+    progress.status = OperationStatus.SUCCESS
     return progress
 
 
 @resource.handler(Action.DELETE)
-def delete_handler(
-    session: Optional[SessionProxy],
-    request: ResourceHandlerRequest,
-    callback_context: MutableMapping[str, Any],
-) -> ProgressEvent:
+def delete_handler(session: Optional[SessionProxy],
+                   request: ResourceHandlerRequest,
+                   callback_context: MutableMapping[str, Any]) -> ProgressEvent:
     model = request.desiredResourceState
     progress: ProgressEvent = ProgressEvent(
         status=OperationStatus.IN_PROGRESS,
         resourceModel=model,
     )
-    # TODO: put code here
+    try:
+        api = APIGateway(session)
+        response = api.delete_api(model.Name)
+        progress.status = OperationStatus.SUCCESS
+        progress.resourceModel = {'Response': response}
+    except TypeError as e:
+        raise exceptions.InternalFailure(f"was not expecting type {e}")
     return progress
 
 
 @resource.handler(Action.READ)
-def read_handler(
-    session: Optional[SessionProxy],
-    request: ResourceHandlerRequest,
-    callback_context: MutableMapping[str, Any],
-) -> ProgressEvent:
+def read_handler(session: Optional[SessionProxy],
+                 request: ResourceHandlerRequest,
+                 callback_context: MutableMapping[str, Any]) -> ProgressEvent:
     model = request.desiredResourceState
-    # TODO: put code here
-    return ProgressEvent(
-        status=OperationStatus.SUCCESS,
+    progress: ProgressEvent = ProgressEvent(
+        status=OperationStatus.IN_PROGRESS,
         resourceModel=model,
     )
+    try:
+        api = APIGateway(session)
+        response = api.get_api(model.Name)
+        progress.status = OperationStatus.SUCCESS
+        progress.resourceModel = {'Response': response}
+    except TypeError as e:
+        raise exceptions.InternalFailure(f"was not expecting type {e}")
+    return progress
 
-
-@resource.handler(Action.LIST)
-def list_handler(
-    session: Optional[SessionProxy],
-    request: ResourceHandlerRequest,
-    callback_context: MutableMapping[str, Any],
-) -> ProgressEvent:
-    # TODO: put code here
-    return ProgressEvent(
-        status=OperationStatus.SUCCESS,
-        resourceModels=[],
-    )
+#
+# @resource.handler(Action.LIST)
+# def list_handler(
+#     session: Optional[SessionProxy],
+#     request: ResourceHandlerRequest,
+#     callback_context: MutableMapping[str, Any],
+# ) -> ProgressEvent:
+#     # TODO: put code here
+#     return ProgressEvent(
+#         status=OperationStatus.SUCCESS,
+#         resourceModels=[],
+#     )
